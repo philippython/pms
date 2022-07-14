e69fcbacc2c057df8844082cd577a13b2f08dd26
Samuel George <georgesam.iv@gmail.com>
2022-07-14 21:54:04 +0100

    feat: Add app first setup
    Our app is a module flaskr

---
+++ flaskr/__init__.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    jsonify,
+)
+import requests
+
+from models import setup_db, db, Users, Countries
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+import sys
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+    CORS(app)
+
+
+    @app.route('/')
+    def index():
+        pass
+
+    # Launch.
+
+    # Default port:
+    if __name__ == '__main__':
+        app.run(debug=True)
+
+
+    return app
